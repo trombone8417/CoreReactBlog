9a9dcd5f3d513a236453367e9b43e9630267b49d
trombone8417 <trombone8417@yahoo.com>
2021-06-22 05:29:54 +0800

    ActivityForm

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,20 +1,21 @@
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../models/activity";
-interface Props{
-    activities:Activity[];
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+interface Props {
+  activities: Activity[];
 }
-export default function ActivityDashboard({activities}:Props) {
-    return (
-        <Grid>
-            <Grid.Column width='10'>
-            <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
-            </Grid.Column>
-        </Grid>
-    )
+export default function ActivityDashboard({ activities }: Props) {
+  return (
+    <Grid>
+      <Grid.Column width='10'>
+        <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width='6'>
+        {activities[0] && <ActivityDetails activity={activities[0]}/>}
+        <ActivityForm />
+      </Grid.Column>
+    </Grid>
+  )
 }
