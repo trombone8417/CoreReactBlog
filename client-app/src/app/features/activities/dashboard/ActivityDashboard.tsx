d2e8403998e19e172b9a76cda0eb67e29dfdcc72
trombone8417 <trombone8417@yahoo.com>
2021-06-27 07:37:07 +0800

    details

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,7 @@
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../../layout/LoadingComponent";
 import { useStore } from "../../../stores/store";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
@@ -8,6 +10,12 @@ import ActivityList from "./ActivityList";
 export default observer(function ActivityDashboard() {
   const { activityStore} = useStore();
   const {selectedActivity,editMode}=activityStore;
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore])
+
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
   return (
     <Grid>
       <Grid.Column width='10'>
