b93433acfe8cb307ff65d81a55f995dca7d9013a
trombone8417 <trombone8417@yahoo.com>
2021-06-27 15:05:46 +0800

    Routing

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,16 +3,14 @@ import { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../layout/LoadingComponent";
 import { useStore } from "../../../stores/store";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 export default observer(function ActivityDashboard() {
-  const { activityStore} = useStore();
-  const {selectedActivity,editMode}=activityStore;
+  const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
   useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
+    if (activityRegistry.size <= 0) loadActivities();
+  }, [activityRegistry.size,loadActivities])
 
   if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
@@ -22,10 +20,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width='6'>
-        {selectedActivity && !editMode &&
-          <ActivityDetails />}
-        {editMode &&
-          <ActivityForm />}
+        <h2>Activity filters</h2>
       </Grid.Column>
     </Grid>
   )
