e881bd614bc42b76db6e8812aa14875c36ff57e0
trombone8417 <trombone8417@yahoo.com>
2021-07-20 06:41:14 +0800

    followers react

--- client-app/src/app/features/profiles/ProfileContent.tsx
+++ client-app/src/app/features/profiles/ProfileContent.tsx
@@ -1,6 +1,8 @@
 import { observer } from "mobx-react-lite";
 import { Tab } from "semantic-ui-react";
 import { Profile } from "../../models/profile";
+import { useStore } from "../../stores/store";
+import ProfileFollowings from "./ProfileFollowings";
 import ProfilePhotos from "./ProfilePhotos";
 
 interface Props {
@@ -8,13 +10,13 @@ interface Props {
 }
 
 export default observer(function ProfileContent({profile}:Props){
-
+    const {profileStore} = useStore();
     const panes = [
         {menuItem:'About',render:()=><Tab.Pane>About Content</Tab.Pane>},
         {menuItem:'Photos',render:()=><ProfilePhotos profile={profile} />},
         {menuItem:'Events',render:()=><Tab.Pane>Events Content</Tab.Pane>},
-        {menuItem:'Followers',render:()=><Tab.Pane>Followers Content</Tab.Pane>},
-        {menuItem:'Following',render:()=><Tab.Pane>Following Content</Tab.Pane>}
+        {menuItem:'Followers',render:()=><ProfileFollowings />},
+        {menuItem:'Following',render:()=><ProfileFollowings />}
     ];
 
     return(
@@ -22,6 +24,7 @@ export default observer(function ProfileContent({profile}:Props){
             menu={{fluid:true,vertical:true}}
             menuPosition='right'
             panes={panes}
+            onTabChange={(e,data) => profileStore.setActiveTab(data.activeIndex)}
         />
     )
 })
