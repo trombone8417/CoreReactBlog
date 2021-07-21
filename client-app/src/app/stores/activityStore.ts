709c7791254e7068462bc38d1ec2798064b71064
trombone8417 <trombone8417@yahoo.com>
2021-07-22 05:48:38 +0800

    react-infinite-scroller

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -4,15 +4,29 @@ import { Activity, ActivityFormValues } from "../models/activity";
 import {format} from "date-fns";
 import { store } from "./store";
 import { Profile } from "../models/profile";
+import { Pagination, PagingParams } from "../models/pagination";
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
     loadingInitial = false;
+    pagination: Pagination | null = null;
+    pagingParams = new PagingParams();
 
     constructor() {
         makeAutoObservable(this)
+    }
+
+    setPagingParams=(pagingParams: PagingParams)=>{
+        this.pagingParams =pagingParams;
+    }
+
+    get axiosParams(){
+        const params = new URLSearchParams();
+        params.append('pageNumber', this.pagingParams.pageNumber.toString());
+        params.append('pageSize', this.pagingParams.pageSize.toString());
+        return params;
     }
 
     get activitiesByDate() {
@@ -33,16 +47,21 @@ export default class ActivityStore {
     loadActivities = async () => {
         this.loadingInitial=true;
         try {
-            const activities = await agent.Activities.list();
-            activities.forEach((activity) => {
+            const result = await agent.Activities.list(this.axiosParams);
+            result.data.forEach((activity) => {
                 this.setActivity(activity);
             })
+            this.setPagination(result.pagination)
             this.setLoadingInitial(false);
 
         } catch (error) {
             console.log(error);
             this.setLoadingInitial(false);
         }
+    }
+
+    setPagination=(pagination:Pagination)=>{
+        this.pagination = pagination;
     }
 
     loadActivity = async(id:string)=>{
